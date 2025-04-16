def294f5236fddafb9ac866e5094279d0fe353b8
jruiz002 <josegrg04@gmail.com>
2025-04-16 12:58:14 -0600

    feat: :sparkles: the client structure and also the client input were added

--- easystore-backend/src/modules/client/client.module.ts
+++ easystore-backend/src/modules/client/client.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { ClientResolver } from './client.resolver';
+import { RegisterClientHandler } from './commands/handlers/register-client.handler';
+import { ClientRepository } from './client.repository';
+import { PrismaService } from '../../infrastructure/database/postgre/postgre'
+
+const CommandHandlers = [RegisterClientHandler];
 
 @Module({
-  imports: [],
-  providers: [ClientResolver],
+  imports: [CqrsModule],
+  providers: [
+    ClientResolver,
+    ClientRepository,
+    PrismaService,
+    ...CommandHandlers,
+  ],
 })
 export class ClientModule {}
