be30b602ced72e213804b75b3850d9657699a308
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2025-04-21 18:22:30 -0600

    feat: :construction: implement login command

--- easystore-backend/src/modules/client/client.module.ts
+++ easystore-backend/src/modules/client/client.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ClientResolver } from './client.resolver';
 import { ClientService } from './client.service';
-import { RegisterClientHandler } from './commands/client.command.handler';
+import {
+  LoginClientHandler,
+  RegisterClientHandler,
+} from './commands/client.command.handler';
 import { FindClientByEmailHandler } from './queries/client.query.handler';
 import { Postgre } from '@/infrastructure/database/postgre/postgre';
+import { LoginClientCommand } from './commands/client.command';
 
 @Module({
   imports: [CqrsModule],
@@ -14,6 +18,7 @@ import { Postgre } from '@/infrastructure/database/postgre/postgre';
     Postgre,
     RegisterClientHandler,
     FindClientByEmailHandler,
+    LoginClientHandler,
   ],
   //exports:[ClientResolver]
 })
