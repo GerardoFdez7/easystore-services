c94aecc3d3daf5101535476a7080a52160c16868
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-04-22 04:46:35 -0600

    feat: :sparkles: add query of business name
    A query was added to search by business name

--- easystore-backend/src/modules/client/queries/client.query.ts
+++ easystore-backend/src/modules/client/queries/client.query.ts
@@ -1,3 +1,7 @@
 export class FindClientByEmailQuery {
   constructor(public readonly email: string) {}
 }
+
+export class FindClientByBusinessNameQuery {
+  constructor(public readonly businessName: string) {}
+}
