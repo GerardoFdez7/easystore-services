1c27bc561f02d4cbea7f8fa7b3c94b7f6c627381
jruiz002 <josegrg04@gmail.com>
2025-05-05 21:07:41 -0600

    feat: :sparkles: added a implementation modified of login handler

--- easystore-backend/src/domains/tenant/aplication/queries/login/login.handler.ts
+++ easystore-backend/src/domains/tenant/aplication/queries/login/login.handler.ts
@@ -1,36 +1,55 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { ITenantRepository } from '../../../aggregates/repositories/tenant.interface';
 import { TenantLoginDTO } from './login.dto';
-import { Email, BusinessName } from '../../../aggregates/value-objects/index';
+import { Email } from '../../../aggregates/value-objects';
 import * as bcrypt from 'bcrypt';
 import { Inject } from '@nestjs/common';
+import { LoggerService } from '@shared/winston/winston.service';
+import {
+  ExceptionHandler,
+  DomainErrorCode,
+} from '../../../../../shared/domains/auth/exception-handler';
 
 @QueryHandler(TenantLoginDTO)
 export class LoginTenantHandler implements IQueryHandler<TenantLoginDTO> {
+  private readonly exceptionHandler: ExceptionHandler;
+
   constructor(
     @Inject('TenantRepository')
     private readonly tenantRepository: ITenantRepository,
-  ) {}
+    private readonly logger: LoggerService,
+  ) {
+    this.exceptionHandler = new ExceptionHandler(this.logger);
+  }
 
   async execute(command: TenantLoginDTO): Promise<boolean> {
     const { identifier, password } = command;
 
-    let tenant = await this.tenantRepository.findByEmail(
+    const tenant = await this.tenantRepository.findByEmail(
       Email.create(identifier),
     );
     if (!tenant) {
-      tenant = await this.tenantRepository.findByBusinessName(
-        BusinessName.create(identifier),
+      this.exceptionHandler.handle(
+        'AuthenticationError',
+        'Tenant with provided email was not found.',
+        DomainErrorCode.TENANT_NOT_FOUND,
+        { 'X-Error-Type': 'TenantAuth' },
       );
     }
-
-    if (!tenant) throw new Error('Tenant not found');
 
     const isPasswordValid = await bcrypt.compare(
       password,
       tenant.get('password').getValue(),
     );
-    if (!isPasswordValid) throw new Error('Invalid credentials');
+
+    if (!isPasswordValid) {
+      this.exceptionHandler.handle(
+        'AuthenticationError',
+        'Invalid credentials provided.',
+        DomainErrorCode.INVALID_CREDENTIALS,
+        { 'X-Error-Type': 'TenantAuth' },
+      );
+    }
 
     return true;
   }
