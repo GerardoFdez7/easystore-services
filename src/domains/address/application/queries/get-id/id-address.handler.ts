2eb6b2754cd21d748f2841b90f25bf3cbc1637c5
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2025-06-26 17:00:04 -0600

    feat: :sparkles: add getAddressById for Address-domain

---
+++ src/domains/address/application/queries/get-id/id-address.handler.ts
@@ -0,0 +1,23 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { Inject, NotFoundException } from '@nestjs/common';
+import IAddressRepository from '../../../aggregates/repositories/address.interface';
+import { Id } from '@domains/value-objects';
+import { AddressMapper, AddressDTO } from '../../mappers';
+import { GetAddressIdDto } from './id-address.dto';
+
+@QueryHandler(GetAddressIdDto)
+export class GetAddressByIdHandler implements IQueryHandler<GetAddressIdDto> {
+  constructor(
+    @Inject('IAddressRepository')
+    private readonly addressRepository: IAddressRepository,
+  ) {}
+
+  async execute(query: GetAddressIdDto): Promise<AddressDTO> {
+    const address = await this.addressRepository.findById(Id.create(query.id));
+    if (!address) {
+      throw new NotFoundException(`Address with id ${query.id} not found`);
+    }
+
+    return AddressMapper.toDto(address);
+  }
+}
