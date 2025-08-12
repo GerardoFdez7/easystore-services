e5444891fe367912492cf67d11e85e5764355bbf
jruiz002 <josegrg04@gmail.com>
2025-08-12 14:23:29 -0600

    feat(get-in-touch): :sparkles: service to send email to owner app implemented

--- src/domains/authentication/infrastructure/emails/index.ts
+++ src/domains/authentication/infrastructure/emails/index.ts
@@ -1,2 +1,3 @@
 export { AuthEmailService } from './auth-email.service';
 export { ForgotPasswordEmailBuilder } from './forgot-password/forgot-password-email.builder';
+export { GetInTouchEmailBuilder } from './get-in-touch/get-in-touch.builder';
