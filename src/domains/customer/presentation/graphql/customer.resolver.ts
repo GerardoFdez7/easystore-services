16431cb952aece11a715ea464649718dea89a6fc
jruiz002 <josegrg04@gmail.com>
2025-10-25 21:07:25 -0600

    feat(wishlist): add mutation to remove variant from wishlist
    Implement mutation endpoint to allow customers to remove items from their wishlist

--- src/domains/customer/presentation/graphql/customer.resolver.ts
+++ src/domains/customer/presentation/graphql/customer.resolver.ts
@@ -4,12 +4,14 @@ import {
   CustomerType,
   UpdateCustomerInput,
   WishListItemCreateInput,
+  WishListItemDeleteInput,
   WishListType,
 } from './types/customer.types';
 import { CurrentUser, JwtPayload } from '@common/decorators';
 import { FindCustomerByIdDto } from '../../application/queries/one/find-customer-by-id.dto';
 import { UpdateCustomerDto } from '../../application/commands/update/update-customer.dto';
 import { CreateWishListDto } from '../../application/commands/create/wish-list/create-wish-list.dto';
+import { DeleteWishListDto } from '../../application/commands/delete/wish-list/delete-wish-list.dto';
 
 @Resolver(() => CustomerType)
 export class CustomerResolver {
@@ -44,6 +46,18 @@ export class CustomerResolver {
         user.tenantId,
       ),
     );
+  }
+
+  @Mutation(() => Boolean)
+  async removeVariantFromWishList(
+    @Args('input', { type: () => WishListItemDeleteInput })
+    input: WishListItemDeleteInput,
+    @CurrentUser() user: JwtPayload,
+  ): Promise<boolean> {
+    await this.commandBus.execute(
+      new DeleteWishListDto(user.customerId, input.variantId, user.tenantId),
+    );
+    return true;
   }
 
   ///////////////
