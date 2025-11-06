e61ad4794440c6777757910273945a02a132a601
jruiz002 <josegrg04@gmail.com>
2025-11-05 22:30:27 -0600

    fix(customer-review-product): validate only required fields in fromPersistence
    Ensure only necessary fields are validated when creating from persistence to avoid potential validation errors with extra properties

--- src/domains/customer/aggregates/value-objects/customer-review-product.vo.ts
+++ src/domains/customer/aggregates/value-objects/customer-review-product.vo.ts
@@ -50,7 +50,12 @@ export class CustomerReviewProduct {
   static fromPersistence(
     props: CustomerReviewProductPropsWithId,
   ): CustomerReviewProduct {
-    customerReviewProductSchema.parse(props);
+    customerReviewProductSchema.parse({
+      ratingCount: props.ratingCount,
+      comment: props.comment,
+      customerId: props.customerId,
+      variantId: props.variantId,
+    });
     return new CustomerReviewProduct(props, props.id, props.updatedAt);
   }
 
